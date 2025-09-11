953373225f4d4f8517634bf4b5d385aa724c4818
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-09-11 20:55:49 -0300

    Utilizando suspense API

--- aula-11/src/App.tsx
+++ aula-11/src/App.tsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape/Rodape";
+import { lazy, Suspense } from "react";
 
+const OutletsPage = lazy(() => import("react-router-dom").then(module => ({ default: module.Outlet })));
 
 export default function App() {
 
   return (
-   
+    
     <div className="container">
       <Cabecalho />
-      <Outlet />
+      <Suspense fallback={<div>Loading...</div>}>  
+        <OutletsPage />
+      </Suspense>
       <Rodape />
     </div>
    
