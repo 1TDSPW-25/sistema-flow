16dba2885d35743127a9757250b384e4864e0275
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-09-25 19:45:10 -0300

    fix: update react-hook-form version and refactor EditarProduto component
    - Updated the version of react-hook-form from 7.62.0 to 7.63.0 in package.json.
    - Removed unused state management for produto and replaced it with react-hook-form's useForm for better form handling.
    - Added document title change for the EditarProduto page.
    - Updated API URL to use environment variable for better configuration.
    - Refactored form submission to use react-hook-form's handleSubmit and reset methods.
    - Cleaned up input fields to remove direct state management and utilize react-hook-form's registration.

--- aula-14/src/routes/EditarProduto/index.tsx
+++ aula-14/src/routes/EditarProduto/index.tsx
@@ -1,64 +1,67 @@
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import type { ProdutoType } from "../../types/produto";
+import { useForm } from "react-hook-form";
+const URL_API = import.meta.env.VITE_API_URL_BASE;
 
 export default function EditarProduto() {
 
+    //Adicionando título à página
+    useEffect(() => {
+      document.title = 'Editar Produto';
+    }, []);
+        
+    //Recebendo o id da URL, que é enviado pelo componente Link da ListagemProdutos.
     const { id } = useParams<string>();
-
+    //Criando um navegador para redirecionar o usuário após a edição do produto.
     const navigate = useNavigate();
 
-    const [produto, setProduto] = useState<ProdutoType>({
-        id: 0,
-        nome: '',
-        preco: 0
+    const {reset,register,handleSubmit,formState:{errors}} = useForm<ProdutoType>({
+        mode: "onChange"
     });
 
+    //Controlando o estado do produto
     useEffect(() => {
         const fetchProduto = async () => {
-            const response = await fetch(`http://localhost:3001/produtos/${id}`);
+            const response = await fetch(`${URL_API}/${id}`);
             const data:ProdutoType = await response.json();
-            setProduto(data);
+            reset(data);
         };
 
         fetchProduto();
-    }, [id]);
+    }, [id, reset]);
 
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
+    const onSubmit = (data:ProdutoType) => {
         // Lógica para enviar os dados atualizados para o servidor
         const atualizaProduto = async () => {
-            await fetch(`http://localhost:3001/produtos/${id}`, {
+            await fetch(`${URL_API}/${id}`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(produto)
+                body: JSON.stringify(data)
             });
             alert('Produto atualizado com sucesso!');
+            // Redirecionar para a página de listagem de produtos após a atualização
+            navigate('/produtos');
         }
         atualizaProduto();
-        
-        // Redirecionar para a página de listagem de produtos após a atualização
-        navigate('/produtos');
-
     }
-
 
     return (
         <main>
             <div className="w-[50vw] h-[50vh] p-7 bg-amber-500 my-10 mx-auto border-2 rounded-2xl">
-                <form onSubmit={handleSubmit} className="w-full h-full">
+                <form className="w-full h-full">
                     <fieldset>
                         <legend className="text-center block w-full font-bold">Produtos</legend>
                         <div>
                             <label htmlFor="idNome" className="font-bold block">Produto : </label>
-                            <input type="text" name="nome" id="idNome" value={produto?.nome} className="border-2 rounded-[5px] bg-amber-50 p-1 mb-5 w-90" onChange={(e) => setProduto({ ...produto, nome: e.target.value })}/>
+                            <input type="text" name="nome" id="idNome"  className="border-2 rounded-[5px] bg-amber-50 p-1 mb-5 w-90"/>
                         </div>
-                        <div>
+                        <div> 
                             <label htmlFor="idPreco" className="font-bold block">R$: </label>
-                            <input type="number" name="preco" id="idPreco" value={produto?.preco} className="border-2 rounded-[5px] bg-amber-50 p-1 mb-5 w-90" onChange={(e) => setProduto({ ...produto, preco: Number(e.target.value) })}/>
+                            <input type="number" name="preco" id="idPreco"  className="border-2 rounded-[5px] bg-amber-50 p-1 mb-5 w-90"/>
                         </div>
                         <div>
                             <button className="bg-green-600 border-2 rounded-[5px] border-white w-40 h-15 my-2 mx-auto block hover:bg-amber-300 hover:text-white">Editar</button>
