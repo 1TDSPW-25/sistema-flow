330504a09ad0540eff745d5cce00aceb2d197481
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-09-25 20:07:38 -0300

    feat: update product details in db.json and enhance form validation in EditarProduto component

--- aula-14/src/routes/EditarProduto/index.tsx
+++ aula-14/src/routes/EditarProduto/index.tsx
@@ -52,16 +52,20 @@ export default function EditarProduto() {
     return (
         <main>
             <div className="w-[50vw] h-[50vh] p-7 bg-amber-500 my-10 mx-auto border-2 rounded-2xl">
-                <form className="w-full h-full">
+                <form onSubmit={handleSubmit(onSubmit)} className="w-full h-full">
                     <fieldset>
                         <legend className="text-center block w-full font-bold">Produtos</legend>
                         <div>
                             <label htmlFor="idNome" className="font-bold block">Produto : </label>
-                            <input type="text" id="idNome"  className="border-2 rounded-[5px] bg-amber-50 p-1 mb-5 w-90" {...register("nome",{required:true})}/>
+                            <input type="text" id="idNome"  className="border-2 rounded-[5px] bg-amber-50 p-1 mb-5 w-90" {...register("nome",{required:true, maxLength:50,minLength:3})} 
+                            aria-invalid={!!errors.nome} aria-describedby={errors.nome ? "nome-error" : undefined}/>
+                            {errors.nome && <span id="nome-error" className="text-red-500 bg-red-300 p-2 rounded border-2 border-red-500 block text-center">Respeite o número de caracteres!</span>}
+
                         </div>
                         <div> 
                             <label htmlFor="idPreco" className="font-bold block">R$: </label>
-                            <input type="number" id="idPreco" step={0.01} className="border-2 rounded-[5px] bg-amber-50 p-1 mb-5 w-90" {...register("preco",{required:true, valueAsNumber:true})}/>
+                            <input type="number" id="idPreco" step={0.01} className="border-2 rounded-[5px] bg-amber-50 p-1 mb-5 w-90" {...register("preco",{required:true, valueAsNumber:true, min:0.01})} aria-invalid={!!errors.preco} aria-describedby={errors.preco ? "preco-error" : undefined}/>
+                            {errors.preco && <span id="preco-error" className="text-red-500 bg-red-300 p-2 rounded border-2 border-red-500 block text-center">Informe um preço válido!</span>}
                         </div>
                         <div>
                             <button className="bg-green-600 border-2 rounded-[5px] border-white w-40 h-15 my-2 mx-auto block hover:bg-amber-300 hover:text-white">Editar</button>
