9d5b9b51c1265ec025e10f4ee268e7dbf295653e
laura853 <lauralopess2006@gmail.com>
2025-10-09 20:29:57 -0300

    feat: criada a request de news

--- sistema-flow/src/routes/Home/index.tsx
+++ sistema-flow/src/routes/Home/index.tsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
-    return(
+
+    const [news, setNews] = useState([])
+
+    useEffect(() => {
+        const url = 'https://newsapi.org/v2/top-headlines?' +
+            'country=br' +
+            'apiKey=0596354ecea24b1c93f25943b4f8dd9e';
+        const req = async () => {
+            const getNews = await fetch(url)
+            const newsResponse = await getNews.json();
+            setNews(newsResponse)
+        };
+        req()
+    }, [])
+    console.log(news)
+    return (
         <main>
             <h1>Home</h1>
         </main>
