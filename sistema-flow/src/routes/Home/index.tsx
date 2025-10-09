40647f7973a63765a877bb179c2e2195597401cf
iagoDAinette <iagodiasainette2302@gmail.com>
2025-10-09 20:13:03 -0300

    create: Home > index.tsx

--- sistema-flow/src/routes/Home/index.tsx
+++ sistema-flow/src/routes/Home/index.tsx
@@ -1,24 +1,7 @@
-import { useEffect, useState } from "react";
-
 export default function Home() {
-
-    const [news, setNews] = useState([])
-
-    useEffect(() => {
-        const url = 'https://newsapi.org/v2/top-headlines?' +
-            'country=br' +
-            'apiKey=0596354ecea24b1c93f25943b4f8dd9e';
-        const req = async () => {
-            const getNews = await fetch(url)
-            const newsResponse = await getNews.json();
-            setNews(newsResponse)
-        };
-        req()
-    }, [])
-    console.log(news)
-    return (
+    return(
         <main>
             <h1>Home</h1>
         </main>
-    );
+    )
 }
