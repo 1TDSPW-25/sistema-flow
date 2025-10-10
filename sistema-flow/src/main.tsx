5d930c87e8bcde75f2487fb4d1bc4c16e6716eef
iagoDAinette <iagodiasainette2302@gmail.com>
2025-10-09 22:12:29 -0300

    feat: create rota Home na Main

--- sistema-flow/src/main.tsx
+++ sistema-flow/src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 import Cad from './routes/Cad/index.tsx'
+import Home from './routes/Home/index.tsx'
 
 const router = createBrowserRouter([
   {path: "/cadastro", element: <Cad/>},  
+  {path: "/home", element: <Home/>},
 ])
 
 createRoot(document.getElementById('root')!).render(
