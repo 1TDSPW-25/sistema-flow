70770395aac889ee43b2f9c113afd2639070d064
Lucas06-ux <lubgian@gmail.com>
2025-10-09 21:51:06 -0300

    feat: alteração de rotas para trota principal de login

--- sistema-flow/src/main.tsx
+++ sistema-flow/src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Login from './routes/Login/index.tsx'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  {path: "/", element: <App/>, 
+    children:[
+      {path: "/", element: <Login/>},
+])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
