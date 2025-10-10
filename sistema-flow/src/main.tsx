5225c8d8c1498904bce0dad84baa408ffc32926d
Jounaxis <jgs123098@gmail.com>
2025-10-09 21:46:35 -0300

    feat: Criação do modelo de rotas no App.tsx, e alteração generica na main.tsx

--- sistema-flow/src/main.tsx
+++ sistema-flow/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Cad from './routes/Cad/index.tsx'
+
+const router = createBrowserRouter([
+  {path: "/cadastro", element: <Cad/>},  
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
