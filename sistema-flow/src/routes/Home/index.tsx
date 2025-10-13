20beaec7c814eddcd889e5e3041c9ffe2a791940
allyhere <camilomichelettors@gmail.com>
2025-10-13 19:53:02 -0300

    feat: implementa listagem e seus tipos

--- sistema-flow/src/routes/Home/index.tsx
+++ sistema-flow/src/routes/Home/index.tsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
+import type { Article } from "../../types/home";
+import { Card } from "../../components/Card/Card";
 
 export default function Home() {
+  const [news, setNews] = useState<Array<Article>>([]);
 
-    const [news, setNews] = useState([])
+  useEffect(() => {
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=0596354ecea24b1c93f25943b4f8dd9e`;
 
-    useEffect(() => {
-        const url = 'https://newsapi.org/v2/top-headlines?' +
-            'country=br' +
-            'apiKey=0596354ecea24b1c93f25943b4f8dd9e';
-        const req = async () => {
-            const getNews = await fetch(url)
-            const newsResponse = await getNews.json();
-            setNews(newsResponse)
-        };
-        req()
-    }, [])
-    console.log(news)
-    return (
-        <main>
-            <h1>Home</h1>
-        </main>
-    )
+    const req = async () => {
+      const getNews = await fetch(url);
+      const newsResponse = await getNews.json();
+      setNews(newsResponse.articles);
+    };
+    req();
+  }, []);
+
+  return (
+    <main className="grail [&>*]:px-4">
+      <h1>Home - Notícias</h1>
+      <section className="card-gallery py-2">
+        {news.map((post) => {
+          return <Card {...post} />;
+        })}
+      </section>
+    </main>
+  );
 }
