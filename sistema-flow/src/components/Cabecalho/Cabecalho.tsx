37f13538095ce4787f893cf7aa187fbb4b003b85
Maigol123 <migshenrique040507@gmail.com>
2025-10-15 20:15:10 -0300

    Ajustando cabeçalho

--- sistema-flow/src/components/Cabecalho/Cabecalho.tsx
+++ sistema-flow/src/components/Cabecalho/Cabecalho.tsx
@@ -1,22 +1,10 @@
-import React from "react";
 import Menu from "../Menu/Menu";
-import "./Cabecalho.css";
 
-const Cabecalho: React.FC = () => {
-  return (
-    <header className="cabecalho">
-      <div className="cabecalho-container">
-        {/* Logo ou nome do sistema */}
-        <h1 className="cabecalho-titulo">Sistema Flow</h1>
-
-        {/* Menu de navegação */}
-        <Menu />
-
-        {/* Botão (exemplo) */}
-        <button className="cabecalho-botao">Sair</button>
-      </div>
-    </header>
-  );
-};
-
-export default Cabecalho;
+export default function Cabecalho() {
+    return (
+        <header>
+            <h1>Sistema de produtos</h1>
+            <Menu />
+        </header>
+    );
+}
