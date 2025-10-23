0e942234504679897534a2f004517c83309b7222
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-10-23 19:29:18 -0300

    fix: Mudando o basename para todas as rotas.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sistema-flow/",
+  base: "/sistema-flow",
   plugins: [react(), tailwindcss()],
 })
