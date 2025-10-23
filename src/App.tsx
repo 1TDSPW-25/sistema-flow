0cf42a6aebc5c9fce18bc890abc3585f90acf4a1
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-10-23 20:53:15 -0300

    fix: Corrigindo arquivos que estavam impedindo o deploy de carregar na gh.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Menu from "./components/Menu/Menu"
 import Rodape from "./components/Rodape/Rodape";
-import ApiTester from "./TesterApi";
-import Cabecalho from "./components/Cabecalho/Cabecalho";
 
 export default function App() {
 
   return (
     
     <div className="mx-auto">
-
-        <ApiTester /> {/* Apague esta linha junto do arquivo ApiTester.tsx quando tiver certeza que a API está funcionando e o projeto finalizado. Esta linha serve apenas para testar a comunicação com a API, retornando o resultado no console do navegador. */}
-        <Cabecalho />
+        <Menu />
         <Outlet />
         <Rodape />
     </div>
--- src/context/NoticiasContext.tsx
+++ src/context/NoticiasContext.tsx
@@ -5,7 +5,7 @@ import type { Article } from "../types/home";
 export const NoticiasContext = createContext<Array<Article>>([]);
 
 export const NoticiasProvider = ({ children }: { children: ReactNode }) => {
-  const [news, setNews] = useState([]);
+  const [news, setNews] = useState<Article[] | undefined>([]);
 
   useEffect(() => {
     const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=0596354ecea24b1c93f25943b4f8dd9e`;
@@ -23,6 +23,6 @@ export const NoticiasProvider = ({ children }: { children: ReactNode }) => {
   }, []);
 
   return (
-    <NoticiasContext.Provider value={news}>{children}</NoticiasContext.Provider>
+    <NoticiasContext.Provider value={news || []}>{children}</NoticiasContext.Provider>
   );
 };
