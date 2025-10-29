ef620870ba998baf72ea297332e8971863dc3118
MaiconDouglas-dev <maicon.timot8@gmail.com>
2025-10-29 10:44:39 -0300

    feat: Remoção do cabecalho da pagina de login e cadastro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Rodape from "./components/Rodape/Rodape";
 import Cabecalho from "./components/Cabecalho";
 
 export default function App() {
+  const { pathname } = useLocation();
+
+  const hideChrome =
+    pathname.startsWith("/login") || pathname.startsWith("/cadastro");
 
   return (
     
     <div className="mx-auto">
-
-        <Cabecalho />
+      {!hideChrome && <Cabecalho />}
         <Outlet />
         <Rodape />
     </div>
